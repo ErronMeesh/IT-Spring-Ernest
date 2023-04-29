e50958b0da0f5bf8f376cbba81a0cac89091259d
ErronMeesh <128481536+ErronMeesh@users.noreply.github.com>
2023-04-29 13:27:22 +0300

    adding 3 and 4 att
    adding 3 and 4 att

---
+++ IT 4 весна 4 таск 2атт/IT 4 весна 4 таск 2атт/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace IT_4_весна_4_таск_2атт
+{
+
+    
+    class Athlete
+    {
+        public string surname;
+        public int numCompetitions;
+        public int sumPlaces;
+
+        public Athlete(string surname, int numCompetitions, int sumPlaces)
+        {
+            this.surname = surname;
+            this.numCompetitions = numCompetitions;
+            this.sumPlaces = sumPlaces;
+        }
+
+        public double CalculateQuality()
+        {
+            return numCompetitions / (double)sumPlaces;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Athlete: {surname}");
+            Console.WriteLine($"Number of competitions: {numCompetitions}");
+            Console.WriteLine($"Sum of places: {sumPlaces}");
+            Console.WriteLine($"Quality: {CalculateQuality()}");
+        }
+    }
+
+    // 2nd lvl
+    class ProfessionalAthlete : Athlete
+    {
+        public bool P;
+
+        public ProfessionalAthlete(string surname, int numCompetitions, int sumPlaces, bool P)
+            : base(surname, numCompetitions, sumPlaces)
+        {
+            this.P = P;
+        }
+
+        public new double CalculateQuality()
+        {
+            if (P == true)
+            {
+                return 1.5 * base.CalculateQuality();
+            }
+            else
+            {
+                return base.CalculateQuality();
+            }
+        }
+
+        public new void PrintInfo()
+        {
+            Console.WriteLine($"Professional athlete: {surname}");
+            Console.WriteLine($"Number of competitions: {numCompetitions}");
+            Console.WriteLine($"Sum of places: {sumPlaces}");
+            Console.WriteLine($"P: {P}");
+            Console.WriteLine($"Quality: {CalculateQuality()}");
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Athlete athlete1 = new Athlete("Smith", 10, 30);
+            athlete1.PrintInfo();
+            Console.WriteLine();
+
+            // ProfessionalAthlete
+            ProfessionalAthlete athlete2 = new ProfessionalAthlete("Johnson", 10, 30, true);
+            athlete2.PrintInfo();
+        }
+    }
+
+}
---
+++ ITвесна 3 таск 2атт/ITвесна 3 таск 2атт/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace ITвесна_3_таск_2атт
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+            Square mySquare = new Square(new Point(1, 1), new Segment(1));
+
+            mySquare.Sizing(2);
+            mySquare.Stretching(3);
+            mySquare.Compressing(0.5);
+            mySquare.Rotating(90);
+            mySquare.ChangeColor("blue");
+
+        }
+    }
+
+    class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Segment
+    {
+        public double Length { get; set; }
+
+        public Segment(double length)
+        {
+            Length = length;
+        }
+
+        public Segment(double x, double y)
+        {
+            Length = Math.Sqrt(x * x + y * y);
+        }
+    }
+
+    class Square
+    {
+        public Point Origin { get; set; }
+        public Segment Side { get; set; }
+        public string Color { get; set; }
+
+        public Square(Point origin, Segment side)
+        {
+            Origin = origin;
+            Side = side;
+            Color = "black";
+            Console.WriteLine($"In start color is: {Color}");
+
+        }
+
+        public void Sizing(double factor)
+        {
+            Side.Length *= factor;
+            Console.WriteLine($"Sizing change Length to: {Side.Length}");
+        }
+
+        public void Stretching(double factor)
+        {
+            Origin.X *= factor;
+            Origin.Y *= factor;
+            Side.Length *= factor;
+            Console.WriteLine($"Stretching change Length to: {Side.Length}");
+        }
+
+        public void Compressing(double factor)
+        {
+            Origin.X *= factor;
+            Origin.Y *= factor;
+            Side.Length *= factor;
+            Console.WriteLine($"Compressing change Length to: {Side.Length}");
+            Console.WriteLine($"Checking the original before rotating: {Origin.X}");
+            Console.WriteLine($"Checking the original before rotating: {Origin.Y}");
+        }
+
+        public void Rotating(double angle)
+        {
+            double radians = angle * Math.PI / 180;
+            double cos = Math.Cos(radians);
+            double sin = Math.Sin(radians);
+
+            double newX = Origin.X * cos - Origin.Y * sin;
+            double newY = Origin.X * sin + Origin.Y * cos;
+
+            Origin.X = newX;
+            Origin.Y = newY;
+            Console.WriteLine($"Checking the original after rotating: {Origin.X}");
+            Console.WriteLine($"Checking the original after rotating: {Origin.Y}");
+        }
+
+        public void ChangeColor(string color)
+        {
+            Color = color;
+            Console.WriteLine($"Now color is: {Color}");
+        }
+    }
+
+}
